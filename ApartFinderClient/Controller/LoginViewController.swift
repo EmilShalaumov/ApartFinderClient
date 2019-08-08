a6c7f8ce0724b80828eea301b5807ebe868bf739
Emil Shalaumov <emil.shalaumov@gmail.com>
2019-08-08 23:19:40 +0300

    Continue playing with authentication

---
+++ ApartFinderClient/Controller/LoginViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LoginViewController.swift
+//  ApartFinderClient
+//
+//  Created by Эмиль Шалаумов on 07/08/2019.
+//  Copyright © 2019 Emil Shalaumov. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logInBtnPressed(_ sender: UIButton) {
+        let LOG_IN_URL = AppDelegate.DATA_SERVICE_URL + "/auth/login"
+        
+        let parameters = ["username": usernameTextField.text!,
+                          "password": passwordTextField.text!]
+
+        AF.request(LOG_IN_URL, method: .get, parameters: parameters).responseJSON { response in
+            print(response)
+            
+            if let result = response.value {
+                let jsonData = result as! NSDictionary
+                print(jsonData.value(forKey: "responseString") as! String)
+                if (jsonData.value(forKey: "responseString") as! String) == "success" {
+                    AppDelegate.token = (jsonData.value(forKey: "sessionToken") as! NSDictionary)
+                        .value(forKey: "tokenString") as? String
+                    UIApplication.setRootView(ViewController.instantiate(from: .Main))
+                } else {
+                    let alert = UIAlertController(title: "Login error", message: "Try again.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+}
---
+++ ApartFinderClient/Extension/UIApplication.swift
@@ -0,0 +1,35 @@
+//
+//  UIApplication.swift
+//  ApartFinderClient
+//
+//  Created by Эмиль Шалаумов on 08/08/2019.
+//  Copyright © 2019 Emil Shalaumov. All rights reserved.
+//
+
+import UIKit
+
+extension UIApplication {
+    
+    static var loginAnimation: UIView.AnimationOptions = .transitionFlipFromRight
+    static var logoutAnimation: UIView.AnimationOptions = .transitionCrossDissolve
+    
+    public static func setRootView(_ viewController: UIViewController,
+                                   options: UIView.AnimationOptions = .transitionFlipFromRight,
+                                   animated: Bool = true,
+                                   duration: TimeInterval = 0.5,
+                                   completion: (() -> Void)? = nil) {
+        guard animated else {
+            UIApplication.shared.keyWindow?.rootViewController = viewController
+            return
+        }
+        
+        UIView.transition(with: UIApplication.shared.keyWindow!, duration: duration, options: options, animations: {
+            let oldState = UIView.areAnimationsEnabled
+            UIView.setAnimationsEnabled(false)
+            UIApplication.shared.keyWindow?.rootViewController = viewController
+            UIView.setAnimationsEnabled(oldState)
+        }) { _ in
+            completion?()
+        }
+    }
+}
